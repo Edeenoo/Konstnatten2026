2dade618dbb42078ce3e4e9b5f5a8eaa759e31fe
villevikestam <villevikestam@gmail.com>
2026-09-23 12:22:25 +0200

    dsjnhjsv

--- script.js
+++ script.js
@@ -1,39 +1,34 @@
-   // Alla spakar börjar nere
-        let spakar = ["ner", "ner", "ner", "ner"];
+let spakar = ["ner", "ner", "ner", "ner"];
 
-        function bytSpak(nummer) {
+function bytSpak(nummer) {
 
-            if (spakar[nummer - 1] === "ner") {
-                spakar[nummer - 1] = "upp";
-            } else {
-                spakar[nummer - 1] = "ner";
-            }
+    if (spakar[nummer - 1] === "ner") {
+        spakar[nummer - 1] = "upp";
+    } else {
+        spakar[nummer - 1] = "ner";
+    }
 
-            // Byt bilden
-            let bild = document.getElementById("spak" + nummer);
+    let bild = document.getElementById("spak" + nummer);
 
-            if (spakar[nummer - 1] === "upp") {
-                bild.src = "spakupp.png";
-            } else {
-                bild.src = "spakner.png";
-            }
+    if (spakar[nummer - 1] === "upp") {
+        bild.src = "Bilder/Koridor1/spakupp.png";
+    } else {
+        bild.src = "Bilder/Koridor1/spakner.png";
+    }
 
-            kontrollera();
-        }
+    kontrolleraPussel();
+}
 
+function kontrolleraPussel() {
 
-        function kontrollera() {
-
-            // Rätt kombination: ner, upp, upp, ner
-            if (
-                spakar[0] === "ner" &&
-                spakar[1] === "upp" &&
-                spakar[2] === "upp" &&
-                spakar[3] === "ner"
-            ) {
-                document.getElementById("vidare").style.display = "block";
-            } else {
-                document.getElementById("vidare").style.display = "none";
-            }
-
-        }
+    if (
+        spakar[0] === "upp" &&
+        spakar[1] === "ner" &&
+        spakar[2] === "ner" &&
+        spakar[3] === "upp"
+    ) {
+        document.getElementById("vidare").style.display = "block";
+    } else {
+        document.getElementById("vidare").style.display = "none";
+    }
+}
